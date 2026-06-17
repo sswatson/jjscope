peso/blazingjj

Add dialog module
This module will hold all dialogs, renamed from ui/*_popup.rs

src/ui/dialog/mod.rs
@@ -1,0 +1,10 @@
+/*! The dialog module contains all modal dialogs,
+previously known as popups.
+
+A Component can launch a dialog by sending
+[`ComponentAction::SetPopup(Some(<popup instance>))`](crate::ui::ComponentAction).
+Once launched, a dialog will recieve all input events from the App,
+until it is closed.
+*/
+
+

sswatson/jjscope

src/ui/dialog/mod.rs
@@ -1,0 +1,10 @@
+/*! The dialog module contains all modal dialogs,
+previously known as popups.
+
+A Component can launch a dialog by sending
+[`ComponentAction::SetPopup(Some(<popup instance>))`](crate::ui::ComponentAction).
+Once launched, a dialog will receive all input events from the App,
+until it is closed.
+*/
+
+
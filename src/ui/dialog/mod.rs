713360cd860ec6f9190cfe44fd55ffb3176c7572
Peer Sommerlund <peer.sommerlund@gmail.com>
2025-07-22 06:53:39 +0200

    Add dialog module
    This module will hold all dialogs, renamed from ui/*_popup.rs

---
+++ src/ui/dialog/mod.rs
@@ -0,0 +1,10 @@
+/*! The dialog module contains all modal dialogs,
+previously known as popups.
+
+A Component can launch a dialog by sending
+[`ComponentAction::SetPopup(Some(<popup instance>))`](crate::ui::ComponentAction).
+Once launched, a dialog will receive all input events from the App,
+until it is closed.
+*/
+
+
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,6 +2,7 @@ pub mod bookmark_set_popup;
 pub mod bookmarks_tab;
 pub mod command_popup;
 pub mod commit_show_cache;
+pub mod dialog;
 pub mod files_tab;
 pub mod help_popup;
 pub mod loader_popup;
